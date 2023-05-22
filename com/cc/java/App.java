6118bbb63c8559aafa869da24ba0425808c6df75
Jamkhan11 <jamshid.rahwardak@gmail.com>
2023-05-22 11:16:09 +0200

    Instanziierung Klasse --> Objekt

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,7 +2,25 @@ package com.cc.java;
 
 public class App {
     
-    
+    public static void main(String[] args) {
+        
 
+        /* Instanziierung Klasse --> Objekt */
+        // Cat -- Referenztyp
+        // cat -- Referenzvariable
+
+        Cat cat1 = new Cat();
+        System.out.println("Blick von aussen: " + cat1);
+        cat1.tellYourAddress();
+
+        System.out.println("--------------------");
+
+        Cat cat2 = new Cat();
+        System.out.println("Blick von aussen (auch Batista): " + cat2);
+        cat2.tellYourAddress();
+
+
+    }
+    
 }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,11 @@
+package com.cc.java;
+
+public class Cat {
+
+    // this --> Instanzvariable // selbsreferenz
+    public void tellYourAddress() {
+    System.out.println("Blick von innen: " + this);
+    }
+
+    
+}
