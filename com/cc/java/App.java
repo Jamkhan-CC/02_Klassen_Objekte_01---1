2f3ed0d4769c38871d2029daff02ae8a26728b4b
Jamkhan11 <jamshid.rahwardak@gmail.com>
2023-05-22 12:59:32 +0200

    output

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -9,18 +9,28 @@ public class App {
         // Cat -- Referenztyp
         // cat -- Referenzvariable
 
-        Cat cat1 = new Cat();
-        System.out.println("Blick von aussen: " + cat1);
-        cat1.tellYourAddress();
+        Cat cat1 = new Cat();    // <-- variable deklarieren
+        output("Blick von aussen: " + cat1);
+        output(cat1.tellYourAddress());
 
-        System.out.println("--------------------");
+        // App.output("hi");
+        output("--------------------");
 
         Cat cat2 = new Cat();
-        System.out.println("Blick von aussen (auch Batista): " + cat2);
-        cat2.tellYourAddress();
+        output("Blick von aussen (auch Batista): " + cat2);
+        output(cat2.tellYourAddress());
 
 
     }
-    
+
+ 
+    public static void output(String outStr) {
+        System.out.println(outStr);   
+        
+
+    }
+
+
+
 }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -3,9 +3,10 @@ package com.cc.java;
 public class Cat {
 
     // this --> Instanzvariable // selbsreferenz
-    public void tellYourAddress() {
-    System.out.println("Blick von innen: " + this);
+    public String tellYourAddress() {
+        return ("Blick von innen: " + this);
+    
     }
 
-    
+
 }
